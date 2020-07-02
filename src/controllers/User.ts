05977ee13e5f86136606cf14e227b170c8e8cb39
Hamza Iqbal Mallick <hiqbalmallick@gmail.com>
2020-07-02 15:56:47 +0500

    Get Logged In User API created

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -18,12 +18,25 @@ class UserController {
   }
 
   intializeRoutes = () => {
+    this.router.get(`${this.path}/me`, auth(), this.getLoggedInUser);
+
     this.router.get(`${this.path}/:userId`, auth(), this.getUserById);
   };
 
   getUserById = async (req: Request, res: Response) => {
     try {
       const result = await userService.getByIdAsync({ id: req.params.userId });
+      return res.status(result.status).json(result.getBody());
+    } catch (err) {
+      console.log(err.message);
+      const result = new response(500).setMsg("Server error");
+      return res.status(result.status).json(result.getBody());
+    }
+  };
+
+  getLoggedInUser = async (req: Request, res: Response) => {
+    try {
+      const result = await userService.getByIdAsync(req.body.user);
       return res.status(result.status).json(result.getBody());
     } catch (err) {
       console.log(err.message);
