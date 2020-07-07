abdd5406263afb9dd1cd3adc05d490ce2693c210
Hamza Iqbal Mallick <hiqbalmallick@gmail.com>
2020-07-07 12:43:01 +0500

    testing deployment on heroku by deleting manual build

--- dist/src/constants/emailTemplates.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EMAIL_TEMPLATES = {
-    WELCOME: "welcome",
-    RESET_PASSWORD: "resetPassword",
-};
--- dist/src/constants/userTypes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.USER_TYPES = {
-    ADMIN: "admin",
-    MERCHANT: "merchant",
-    CONSUMER: "consumer",
-};
